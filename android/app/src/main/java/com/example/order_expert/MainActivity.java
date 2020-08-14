1c3efc4b3c931b2a434efa89c4687f2a5500b07c
pmpardeshi <pramodmpardeshi@gmail.com>
2020-08-14 10:03:59 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/order_expert/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.order_expert;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+void main(){
+  runApp(
+    Center(
+      child: Text(
+          "hello world",
+          textDirection: TextDirection.ltr
+      ),
+    ),
+
+  );
+}
